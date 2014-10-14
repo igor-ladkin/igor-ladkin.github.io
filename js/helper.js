72319370a4e54c50d97e543a83ae626afcaea186
Igor Ladkin <dgkplan@googlemail.com>
2014-10-14 23:59:23 +0400

    Add content to all parts of bio. Refactor encapsulation.

--- js/helper.js
+++ js/helper.js
@@ -1,2 +1,13 @@
-var HTMLheaderPhoto 	 = "<div id='photo'><img src='%data%' alt='photo'></div>",
-		HTMLheaderGreeting = "<h1>Hello,<br>my name is %data% and this is my resume</h1>";
+var HTMLheaderPhoto 	 	 	 = "<div id='photo'><img src='%data%' alt='photo'></div>",
+		HTMLheaderGreeting 	 	 = "<h1>Hello,<br>my name is %data% and this is my resume</h1>",
+		HTMLaboutMe				 	 	 = "<p>%data%</p>",
+		HTMLcontactItem	   	 	 = "<div class='contact-item'></div>",
+		HTMLcontactIcon	   	 	 = "<div class='icon'><span class='fa fa-%data%'></span></div>",
+		HTMLcontactTitle   	 	 = "<div class='title'>%data%</div>",
+		HTMLcontactDescription = "<div class='description'>Lorem ipsum dolor.</div>",
+		HTMLskillHeader				 = "<div class='skills'></div>",
+		HTMLskillItem					 = "<div class='item-skills' data-percent='%value%' style='width: %style%px;'>%data%</div>",
+		HTMLlanguageHeader		 = "<div id='language-skills'></div>",
+		HTMLlanguageItem			 = "<div class='skill'>%data%</div>",
+		HTMLlanguageRating		 = "<div class='icons pull-right'><div style='width: %rating%%; height: 14px;' class='icons-red'></div></div>",
+		HTMLhobbyItem 				 = "<div class='hobby'>%data%</div>";
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,13 +11,122 @@ var bio = {
 	},
 	avatar: 'http://placehold.it/150x150',
 	aboutMe: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-	skills: ['Ruby on Rails', 'JavaScript', 'HTML and CSS', 'Board Riding'],
+	skills: [['Ruby on Rails', 0.8], ['JavaScript', 0.65], ['HTML and CSS', 0.55], ['Board Riding', 0.4]],
+	languages: [['Russian', 100], ['English', 80]],
+	hobbies :['Travelling', 'Crossfit', 'Surfing', 'Snowboarding'],
 
 	display: function() {
-		var formattedPhoto 		= HTMLheaderPhoto.replace('%data%', bio.avatar),
-				formattedGreeting = HTMLheaderGreeting.replace('%data%', bio.firstName);
+		var formatAboutMe = function() {
+			var aboutMeSection = $('#about'),
+			formattedAboutMe 	 = HTMLaboutMe.replace('%data%', bio.aboutMe);
 
-		$('#photo-header').append(formattedPhoto).append(formattedGreeting);
+			aboutMeSection.append(formattedAboutMe);
+
+			return [formattedAboutMe];
+		}(),
+		formatHeader = function() {
+			var headerSection = $('#photo-header'),
+			formattedPhoto    = HTMLheaderPhoto.replace('%data%', bio.avatar),
+			formattedGreeting = HTMLheaderGreeting.replace('%data%', bio.firstName);
+
+			headerSection.append(formattedPhoto).append(formattedGreeting);
+
+			return [formattedPhoto, formattedGreeting];
+		}(),
+		formatContacts = function() {
+			var contacts 					= bio.contacts,
+			contactSection 				= $('#contacts'),
+			formattedContactError = '<p>There are no avaliable contacts.</p>', 
+			formattedContactIcon,
+			formattedContactTitle,
+			formattedContactDescription;
+
+			if (Object.keys(contacts).length > 0) {
+				for (var key in contacts) {
+					contactSection.append(HTMLcontactItem);
+
+					formattedContactIcon  			= HTMLcontactIcon.replace('%data%', key);
+					formattedContactTitle 			= HTMLcontactTitle.replace('%data%', key.toUpperCase());
+					formattedContactDescription = HTMLcontactDescription.replace('%data%', bio.contacts[key]);
+
+					$('.contact-item:last').append(formattedContactIcon).append(formattedContactTitle).
+																	append(formattedContactDescription);
+				}
+
+				return [formattedContactIcon, formattedContactTitle, formattedContactDescription];
+			} else {
+				contactSection.append(formattedContactError);
+
+				return formattedContactError;
+			}
+		}(),
+		formatSkills = function() {
+			$('#skills').append(HTMLskillHeader);
+
+			var skills 					= bio.skills,
+			skillSection 				= $('.skills:last'),
+			formattedSkillError = '<p>There are no interesting skills.</p>',
+			formattedSkillItem;
+
+			if (skills.length > 0) {
+				for (var i in skills) {
+					formattedSkillItem = HTMLskillItem.replace('%data%', skills[i][0]).replace('%value%', skills[i][1]).
+																						 replace('%style%', (458 * skills[i][1]).toFixed(1));
+					
+					skillSection.append(formattedSkillItem);
+				}
+
+				return formattedSkillItem;
+			} else {
+				skillSection.append(formattedSkillError);
+
+				return formattedSkillError;
+			}
+		}(),
+		formatLanguages = function() {
+			$('#languages').append(HTMLlanguageHeader);
+
+			var languages 				 = bio.languages,
+			languageSection 			 = $('#language-skills'),
+			formattedLanguageError = '<p>There are no well known languages.</p>',
+			formattedLanguageItem,
+			formattedLanguageRating;
+
+			if (languages.length > 0) {
+				for (var i in languages) {
+					formattedLanguageItem 	= HTMLlanguageItem.replace('%data%', languages[i][0]);
+					formattedLanguageRating = HTMLlanguageRating.replace('%rating%', languages[i][1]);
+
+					languageSection.append(formattedLanguageItem);
+					$('.skill:last').append(formattedLanguageRating);
+				}
+
+				return [formattedLanguageItem, formattedLanguageRating];
+			} else {
+				languageSection.append(formattedLanguageError);
+
+				return formattedLanguageError;
+			}
+		}(),
+		formatHobbies = function() {
+			var hobbies 				= bio.hobbies,
+			hobbySection 			  = $('#hobbies'),
+			formattedHobbyError = '<p>There are no hobbies at all.</p>',
+			formattedHobbyItem;
+
+			if (hobbies.length > 0) {
+				for (var i in hobbies) {
+					formattedHobbyItem = HTMLhobbyItem.replace('%data%', hobbies[i]);
+
+					hobbySection.append(formattedHobbyItem);
+				}
+				return [formattedHobbyItem];
+			} else {
+				hobbySection.append(formattedHobbyError);
+
+				return formattedHobbyError;
+			}
+		}();
 	}
 };
 
