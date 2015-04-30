8d83b097a740ba0bfa3d481309d34705fa753f32
Igor Ladkin <dgkplan@googlemail.com>
2015-04-30 16:35:49 +0300

    Fix google map.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -517,8 +517,8 @@ var map = {
 
     createMapMarker = function(placeData) {
       // The next lines save location data from the search result object to local variables
-      var lat    = placeData.geometry.location.k,  // latitude from the place service
-          lon    = placeData.geometry.location.D,  // longitude from the place service
+      var lat    = placeData.geometry.location.lat(),  // latitude from the place service
+          lon    = placeData.geometry.location.lng(),  // longitude from the place service
           name   = placeData.formatted_address,    // name of the place from the place service
           bounds = window.mapBounds;               // current boundaries of the map window
 
